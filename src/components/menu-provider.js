f36e244691295138cd6a179ec7bc6c238b07265e
leemyunggyu <jaylee@hatiolab.com>
2019-04-05 18:12:24 +0900

    메뉴 데이터 가공 추가

--- src/components/menu-provider.js
+++ src/components/menu-provider.js
@@ -1,6 +1,6 @@
-import { LitElement } from 'lit-element'
-import { store } from '@things-factory/shell'
-import { updateBaseMenu } from '@things-factory/base-menu'
+import { updateBaseMenu } from '@things-factory/base-menu';
+import { store } from '@things-factory/shell';
+import { LitElement } from 'lit-element';
 
 export default class MenuProvider extends LitElement {
   static get properties() {
@@ -8,12 +8,15 @@ export default class MenuProvider extends LitElement {
   }
 
   firstUpdated(changedProperties) {
-    fetch('http://board-demo.hatiolab.com/rest/menus', {
+    var searchParam = new URLSearchParams()
+    searchParam.append('query', JSON.stringify([{ name: 'hidden_flag', operator: 'is_not_true' }]))
+
+    fetch(`http://52.231.75.202/rest/menus/user_menus/STANDARD?${searchParam}`, {
       credentials: 'include'
     }).then(res => {
       if (res.ok) {
         res.json().then(json => {
-          store.dispatch(updateBaseMenu(this._convertMenu(json.items)))
+          store.dispatch(updateBaseMenu(this._convertMenu(json)))
           return json
         })
       }
@@ -21,12 +24,33 @@ export default class MenuProvider extends LitElement {
   }
 
   _convertMenu(menus = []) {
-    return menus.map(m => {
-      return {
-        name: m.name,
-        pageName: m.routing
+    const menuObject = {}
+    const menuList = []
+    menus.forEach(m => {
+      if((m.menu_type || '').toUpperCase() === 'MENU') {
+        menuObject[m.id] = Object.assign({}, menuObject[m.id], {
+          rank: m.rank,
+          name: m.title
+        })
+      } else {
+        if(!menuObject[m.parent_id]) 
+          menuObject[m.parent_id] = {
+            children: []    
+          }
+
+        menuObject[m.parent_id].children.push({
+          rank: m.rank,
+          name: m.title,
+          pageName: m.routing
+        })
       }
     })
+
+    Object.values(menuObject).forEach(v => {
+      menuList.push(v)
+    })
+
+    return menuList
   }
 }
 
